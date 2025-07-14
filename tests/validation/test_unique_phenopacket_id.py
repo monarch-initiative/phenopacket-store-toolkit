f6d816b1eda94f270867a1c620eab9bd13af0705
Daniel Danis <daniel.gordon.danis@protonmail.com>
2025-07-14 09:49:49 +0200

    Update `UniquePhenopacketId` tests.

--- tests/validation/test_unique_phenopacket_id.py
+++ tests/validation/test_unique_phenopacket_id.py
@@ -1,41 +1,70 @@
-from ppktstore.validation import UniquePhenopacketId, PhenopacketStoreAuditor
-from ppktstore.model import CohortInfo, DefaultPhenopacketStore, EagerPhenopacketInfo
+import pathlib
+import typing
+
+import pytest
+
+from ppktstore.validation import UniquePhenopacketId
+from ppktstore.model import CohortInfo, PhenopacketStore, EagerPhenopacketInfo
 from phenopackets.schema.v2.phenopackets_pb2 import Phenopacket
 
 
-def make_phenopacket(id: str):
+def make_phenopacket(
+    id: str,
+) -> Phenopacket:
     pp = Phenopacket()
     pp.id = id
     return pp
 
 
-def make_cohort(name: str, ids):
-    infos = [EagerPhenopacketInfo.from_phenopacket(str(i), make_phenopacket(i)) for i in ids]
+def make_cohort(
+    name: str,
+    ids: typing.Iterable[str],
+) -> CohortInfo:
+    infos = [
+        EagerPhenopacketInfo.from_phenopacket(str(i), make_phenopacket(i)) for i in ids
+    ]
     return CohortInfo(name, name, infos)
 
 
-def make_store(cohorts):
-    return DefaultPhenopacketStore("teststore", None, cohorts)
+def make_store(cohorts) -> PhenopacketStore:
+    return PhenopacketStore.from_cohorts("teststore", pathlib.Path("."), cohorts)
 
 
 class TestUniquePhenopacketId:
+    @pytest.fixture
+    def check(self) -> UniquePhenopacketId:
+        return UniquePhenopacketId()
 
-    def test_unique_phenopacket_id_error(self):
+    def test_unique_phenopacket_id_error(
+        self,
+        check: UniquePhenopacketId,
+    ):
         # Two cohorts, overlapping phenopacket id 'A'
-        cohort1 = make_cohort('cohort1', ['A', 'B'])
-        cohort2 = make_cohort('cohort2', ['A', 'C'])
+        cohort1 = make_cohort("cohort1", ["A", "B"])
+        cohort2 = make_cohort("cohort2", ["A", "C"])
         store = make_store([cohort1, cohort2])
-        auditor = UniquePhenopacketId()
-        notepad = PhenopacketStoreAuditor.prepare_notepad("test-ps")
-        auditor.audit(store, notepad)
+
+        notepad = check.prepare_notepad("test-ps")
+        check.audit(store, notepad)
+
         assert notepad.has_errors_or_warnings(include_subsections=False)
 
-    def test_unique_phenopacket_id_pass(self):
+        errors = tuple(notepad.errors())
+        assert len(errors) == 1
+        error = errors[0]
+        assert error.message == "`A` is present in 2 cohorts: {'cohort1', 'cohort2'}"
+        assert error.solution is None
+
+    def test_unique_phenopacket_id_pass(
+        self,
+        check: UniquePhenopacketId,
+    ):
         # Two cohorts, all ids unique
-        cohort1 = make_cohort('cohort1', ['A', 'B'])
-        cohort2 = make_cohort('cohort2', ['C', 'D'])
+        cohort1 = make_cohort("cohort1", ["A", "B"])
+        cohort2 = make_cohort("cohort2", ["C", "D"])
         store = make_store([cohort1, cohort2])
-        auditor = UniquePhenopacketId()
-        notepad = PhenopacketStoreAuditor.prepare_notepad("test-ps")
-        auditor.audit(store, notepad)
+
+        notepad = check.prepare_notepad("test-ps")
+        check.audit(store, notepad)
+
         assert not notepad.has_errors_or_warnings(include_subsections=False)
