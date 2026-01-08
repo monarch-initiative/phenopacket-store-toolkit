b17870b3217f4a756615e5002ee86de7c805de33
Daniel Danis <daniel.gordon.danis@protonmail.com>
2026-01-08 14:06:35 +0100

    Make `pyproject.toml` the source of truth regarding the version.

--- src/ppktstore/__init__.py
+++ src/ppktstore/__init__.py
@@ -3,12 +3,14 @@ Phenopacket Store Toolkit helps with Phenopacket Store release and Q/C
 and simplifies access to the store data for the downstream applications.
 """
 
+from importlib.metadata import version
+
 from . import registry
 from . import model
 
 # We do not import `.release` package since it requires extra dependencies.
 
-__version__ = "0.1.6.dev0"
+__version__ = version("ppktstore")
 
 __all__ = [
     "registry",
